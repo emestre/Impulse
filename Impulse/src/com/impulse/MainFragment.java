e22781e39245397663b32919783fff00b5a413c9
emestre <eliottmestre@gmail.com>
2014-02-18 22:07:56 -0800

    Facebook Sign In

---
+++ Impulse/src/com/impulse/MainFragment.java
@@ -0,0 +1,119 @@
+package com.impulse;
+
+import java.util.Arrays;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+import com.facebook.widget.ProfilePictureView;
+
+public class MainFragment extends Fragment {
+
+	private static final String TAG = "MainFragment";
+	private UiLifecycleHelper uiHelper;
+	private TextView userName;
+	private ProfilePictureView profPic;
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+		@Override
+		public void call(Session session, SessionState state,
+				Exception exception) {
+			onSessionStateChange(session, state, exception);
+		}
+	};
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    uiHelper = new UiLifecycleHelper(getActivity(), callback);
+	    uiHelper.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.activity_login, container, false);
+		LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+		authButton.setFragment(this);
+		authButton.setReadPermissions(Arrays.asList("user_likes", "user_status"));
+        userName = (TextView) view.findViewById(R.id.user_name);
+        profPic = (ProfilePictureView) view.findViewById(R.id.profile_picture);
+        userName.setVisibility(View.GONE);
+        profPic.setVisibility(View.GONE);
+		return view;
+	}
+	
+	@Override
+	public void onResume() {
+	    super.onResume();
+	    Session session = Session.getActiveSession();
+	    if (session != null &&
+	           (session.isOpened() || session.isClosed()) ) {
+	        onSessionStateChange(session, session.getState(), null);
+	    }
+	    uiHelper.onResume();
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	    uiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onPause() {
+	    super.onPause();
+	    uiHelper.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+	    super.onDestroy();
+	    uiHelper.onDestroy();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+	    super.onSaveInstanceState(outState);
+	    uiHelper.onSaveInstanceState(outState);
+	}
+
+	private void onSessionStateChange(Session session, SessionState state,
+			Exception exception) {
+		if (state.isOpened()) {
+			Log.i(TAG, "Logged in...");
+			 Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+		            // callback after Graph API response with user object
+		            @Override
+		            public void onCompleted(GraphUser user, Response response) {
+		              if (user != null) {
+		                userName.setVisibility(View.VISIBLE);
+		                profPic.setVisibility(View.VISIBLE);
+		                Log.i(TAG, user.getName());
+		                userName.setText(user.getName());
+		                Log.i(TAG, user.getId());
+		                profPic.setProfileId(user.getId());
+		              }
+		            }
+		          }).executeAsync();
+		} else if (state.isClosed()) {
+			Log.i(TAG, "Logged out...");
+            userName.setVisibility(View.GONE);
+            profPic.setVisibility(View.GONE);
+		}
+	}
+}
