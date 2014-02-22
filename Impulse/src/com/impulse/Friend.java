f436493742b854f1b649498779da496db35a6a04
emestre <eliottmestre@gmail.com>
2014-02-22 15:42:23 -0800

    New files

---
+++ Impulse/src/com/impulse/Friend.java
@@ -0,0 +1,23 @@
+package com.impulse;
+
+/**
+ * Created by Eliot on 2/22/14.
+ */
+public class Friend {
+
+    private String user_name;
+    private String user_id;
+
+    public Friend(String user_name, String user_id) {
+        this.user_id = user_id;
+        this.user_name = user_name;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+}
