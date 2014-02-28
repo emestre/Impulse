5fec818d486c3538e61b77a448ef62b00c715074
akshay159 <akshay159@gmail.com>
2014-02-27 17:19:32 -0800

    sending pictures to server added

--- Impulse/src/com/impulse/PostTask.java
+++ Impulse/src/com/impulse/PostTask.java
@@ -3,11 +3,15 @@ package com.impulse;
 import android.os.AsyncTask;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
@@ -18,6 +22,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
@@ -30,7 +35,12 @@ public class PostTask extends AsyncTask<String, String, String> {
     private String url;
     private RestTaskCallback callback;
     private String userKey;
-    private ArrayList<String> friends;
+    private String caption;
+    private double latitude;
+    private double longitude;
+    private String filePath;
+    private String extension;
+    private int timeout;
 
     /**
      * Creates a new instance of PostTask with the specified URL, callback, and
@@ -48,11 +58,17 @@ public class PostTask extends AsyncTask<String, String, String> {
         this.callback = callback;
     }
 
-    public PostTask(String url, ArrayList<String> friends, RestTaskCallback callback) {
-        this.url = url;
-        this.friends = friends;
+    public PostTask(String userKey, String caption, double latitude, double longitude, String filePath, String extension, int timeout, RestTaskCallback callback) {
+        this.userKey = userKey;
+        this.caption = caption;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.filePath = filePath;
+        this.extension = extension;
+        this.timeout = timeout;
         this.callback = callback;
     }
+
 
     @Override
     protected String doInBackground(String... arg0) {
@@ -81,27 +97,26 @@ public class PostTask extends AsyncTask<String, String, String> {
             return "Error Occurred";
         }
 
-        else if (type.equals("/getFriendList")) {
-            String result = null;
-            List<NameValuePair> list = new ArrayList<NameValuePair>();
-            list.add(new BasicNameValuePair("size", friends.size() + ""));
-            for (int i = 0; i < friends.size(); i++)
-                 list.add(new BasicNameValuePair(i + "", friends.get(i)));
+        else if (type.equals("/uploadFile")) {
+            MultipartEntityBuilder entity = MultipartEntityBuilder.create();
+            entity.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+            entity.addTextBody("extension", this.extension);
+            entity.addTextBody("userKey", this.userKey);
+            entity.addTextBody("caption", this.caption);
+            entity.addTextBody("latitude", String.valueOf(this.latitude));
+            entity.addTextBody("longitude", String.valueOf(this.longitude));
+            entity.addTextBody("timeout", String.valueOf(this.timeout));
+            entity.addPart("image", new FileBody(new File(filePath)));
 
-            String paramString = URLEncodedUtils.format(list, "utf-8");
-            url += "?" + paramString;
-            HttpGet get = new HttpGet(url);
             try {
-                resp = client.execute(get);
-                result = (new BasicResponseHandler()).handleResponse(resp);
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
+                post.setEntity(entity.build());
+                resp = client.execute(post);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            if (result != null)
-                return result;
+            if (resp != null)
+                return Integer.toString(resp.getStatusLine().getStatusCode());
 
             return "Error Occurred";
         }
