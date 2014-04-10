25f3e405a9090dd39d4dd77b6a92f200faabe86d
Alex Lin <alexchrislin@gmail.com>
2014-04-10 16:18:23 -0700

    profile UI updated

--- impulse/src/main/java/com/impulse/android/ProfileActivity.java
+++ impulse/src/main/java/com/impulse/android/ProfileActivity.java
@@ -1,14 +1,22 @@
 package com.impulse.android;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.facebook.HttpMethod;
@@ -23,6 +31,10 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.squareup.picasso.Picasso;
 
+import org.joda.time.DateTime;
+import org.joda.time.Years;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -31,74 +43,112 @@ public class ProfileActivity extends Fragment {
 
     private static final String TAG = "ProfileActivity";
 
-    private TextView userName;
-//    private ProfilePictureView profPic;
-    private HorizontalListView friendsList;
-    private ArrayList<Friend> friends;
-    private FriendViewAdapter friendsAdapter;
-    private Button viewPosts;
-    private String userId;
+    private String mUserId;
+
+    private TextView mUserName;
+    private EditText mAboutField;
+    private HorizontalListView mFriendScrollList;
+    private ArrayList<Friend> mFriendList;
+    private FriendViewAdapter mFriendAdapter;
+    private Button mViewPostsButton;
     private TextView mFriendsText;
-    private ImageView profPic;
+    private ImageView mProfilePic;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.activity_profile, container, false);
 
-        friends = new ArrayList<Friend>();
-        friendsAdapter = new FriendViewAdapter(getActivity(), friends);
         final Session session = Session.getActiveSession();
-        userName = (TextView) root.findViewById(R.id.user_name);
-        friendsList = (HorizontalListView) root.findViewById(R.id.friends_list);
-        viewPosts = (Button) root.findViewById(R.id.button_user_posts);
-        friendsList.setAdapter(friendsAdapter);
-        profPic = (ImageView) root.findViewById(R.id.profile_picture);
+
+        mUserName = (TextView) root.findViewById(R.id.user_name);
+        mAboutField = (EditText) root.findViewById(R.id.bio_text);
+        mProfilePic = (ImageView) root.findViewById(R.id.profile_picture);
         mFriendsText = (TextView) root.findViewById(R.id.friends_text_field);
+        mViewPostsButton = (Button) root.findViewById(R.id.button_user_posts);
 
-        Request.newMeRequest(session, new Request.GraphUserCallback() {
+        mFriendList = new ArrayList<Friend>();
+        mFriendAdapter = new FriendViewAdapter(getActivity(), mFriendList);
+        mFriendScrollList = (HorizontalListView) root.findViewById(R.id.friends_list);
+        mFriendScrollList.setAdapter(mFriendAdapter);
 
-            // callback after Graph API response with user object
+        // get the user ID from the bundled arguments to populate profile information
+        mUserId = this.getArguments().getString("id");
+        Log.d(TAG, "user ID: " + mUserId);
+
+        // if we're viewing the logged in user's profile, then allow editing of about field
+        if (mUserId.equals(getActivity().getSharedPreferences("com.impulse",
+                Context.MODE_PRIVATE).getString("UserId", ""))) {
+
+            mAboutField.setEnabled(true);
+        }
+
+        Request.newGraphPathRequest(session, mUserId, new Request.Callback() {
             @Override
-            public void onCompleted(GraphUser user, Response response) {
-                if (user != null) {
-                    // get the facebook user ID
-                    userId = user.getId();
-                    // set this user's name
-                    userName.setText(user.getName().split(" ")[0]);
-                    // get this user's list of friends on impulse
-                    getFriends(session);
-                    // load profile picture
+            public void onCompleted(Response response) {
+                GraphUser user = response.getGraphObjectAs(GraphUser.class);
+
+                // load profile picture
                     Picasso.with(getActivity())
-                            .load("https://graph.facebook.com/" + userId + "/picture?type=large&redirect=true&width=400&height=400")
-                            .into(profPic);
+                            .load("https://graph.facebook.com/" + mUserId + "/picture?type=large&redirect=true&width=400&height=400")
+                            .into(mProfilePic);
+
+                // set user name
+                mUserName.setText(user.getName().split(" ")[0]);
+
+                // get the user's birthday to calculate age
+                if (user.getBirthday() != null) {
+                    DateTimeFormatter formatter = DateTimeFormat.forPattern("MM/dd/yyyy");
+                    DateTime dob = formatter.parseDateTime(user.getBirthday());
+                    DateTime now = new DateTime();
+                    // set this user's age
+                    mUserName.setText(mUserName.getText() + ", " + Years.yearsBetween(dob, now).getYears());
                 }
+                else {
+                    Log.d(TAG, "user's birthday returned NULL");
+                }
+
+                // get this user's list of friends on impulse
+                getFriends(session);
             }
         }).executeAsync();
 
-        friendsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mFriendScrollList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // get the new person's ID
-                userId = ((Friend) friendsList.getItemAtPosition(position)).getUser_id();
-                // set the new user's name
-                userName.setText(((Friend) friendsList.getItemAtPosition(position)).getUser_name());
-                // load new person's profile picture
-                Picasso.with(getActivity())
-                        .load("https://graph.facebook.com/" + userId + "/picture?type=large&redirect=true&width=400&height=400")
-                        .into(profPic);
-                // update to this user's friends list
-                friends.clear();
-                friendsAdapter.notifyDataSetChanged();
-                getFriends(session);
+                // get the new user's ID
+                String newUserId = ((Friend) mFriendScrollList.getItemAtPosition(position)).getUser_id();
+
+                Bundle bundle = new Bundle();
+                bundle.putString("id", newUserId);
+
+                Fragment fragment = new ProfileActivity();
+                fragment.setArguments(bundle);
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.addToBackStack(null);
+                transaction.replace(R.id.content_frame, fragment).commit();
             }
         });
 
-        viewPosts.setOnClickListener(new View.OnClickListener() {
+        mViewPostsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), DrawerActivity.class);
-                intent.putExtra("USER_ID", userId);
+                intent.putExtra("USER_ID", mUserId);
                 startActivity(intent);
+            }
+        });
+
+        LinearLayout screen = (LinearLayout) root.findViewById(R.id.profile_container);
+        // set the root layout to receive touch events to hide keyboard
+        screen.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // hide the keyboard if touch is received outside of keyboard
+                InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
+
+                return true;
             }
         });
 
@@ -107,14 +157,14 @@ public class ProfileActivity extends Fragment {
 
     private void getFriends(Session session) {
         Bundle requestBundle = new Bundle();
-        requestBundle.putString("fields", "id,name,installed");
+        requestBundle.putString("fields", "id, name, installed");
 
-        new Request(session, "/" + userId + "/friends", requestBundle, HttpMethod.GET, new Request.Callback() {
+        new Request(session, "/" + mUserId + "/friends", requestBundle, HttpMethod.GET, new Request.Callback() {
             @Override
             public void onCompleted(Response response) {
                 parseFriends(response);
-                mFriendsText.setText("Friends on Impulse (" + friends.size() + ")");
-                friendsAdapter.notifyDataSetChanged();
+                mFriendsText.setText("Friends on Impulse (" + mFriendList.size() + ")");
+                mFriendAdapter.notifyDataSetChanged();
             }
         }).executeAsync();
     }
@@ -131,7 +181,7 @@ public class ProfileActivity extends Fragment {
             if (newFriend.has("installed") && newFriend.get("installed").getAsString().equals("true")) {
                 String friendName = newFriend.get("name").getAsString().split(" ")[0];
                 String friendId = newFriend.get("id").getAsString();
-                friends.add(new Friend(friendName, friendId));
+                mFriendList.add(new Friend(friendName, friendId));
             }
         }
     }
