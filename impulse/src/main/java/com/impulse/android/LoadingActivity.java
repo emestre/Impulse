1993ee5b375ed9f596ae797d6622edfa03fae88f
Eliot Mestre <eliot.mestre@SL-WL-352.SLO.MBO.AD>
2014-05-03 15:59:32 -0700

    Notifications BIATCH

--- impulse/src/main/java/com/impulse/android/LoadingActivity.java
+++ impulse/src/main/java/com/impulse/android/LoadingActivity.java
@@ -1,5 +1,6 @@
 package com.impulse.android;
 
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.impulse.android.util.SystemUiHider;
 
 import android.annotation.TargetApi;
@@ -7,13 +8,17 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -21,7 +26,16 @@ import android.view.WindowManager;
 import org.apache.http.HttpStatus;
 import org.apache.http.protocol.HTTP;
 
+import java.io.IOException;
+
 public class LoadingActivity extends Activity {
+
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String PROPERTY_REG_ID = "registration_id";
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private String SENDER_ID = "354120366381";
+    private ProgressDialog mUploadingProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,45 +47,27 @@ public class LoadingActivity extends Activity {
         if (Build.VERSION.SDK_INT < 16) {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
-        else {
+        } else {
             ActionBar actionBar = getActionBar();
             actionBar.hide();
         }
 
         // display a loading spinner while the image is uploading
-        final ProgressDialog mUploadingProgress = new ProgressDialog(this);
+        mUploadingProgress = new ProgressDialog(this);
         mUploadingProgress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         mUploadingProgress.setTitle("Loading...");
         mUploadingProgress.setIndeterminate(true);
         mUploadingProgress.setCancelable(false);
         mUploadingProgress.show();
 
-        PackageInfo pInfo = null;
-        String version = null;
-        try {
-            pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-            version = pInfo.versionName;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        String regid = getRegistrationId(getApplicationContext());
 
-        RestClient client = new RestClient();
-        client.initSession(version, new GetCallback() {
-            @Override
-            void onDataReceived(String response) {
-                if(response.equals(HttpStatus.SC_OK + "")) {
-                    mUploadingProgress.dismiss();
-                    Intent intent = new Intent(LoadingActivity.this, DrawerActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(intent);
-                    finish();
-                }
-                else
-                    showIncorrectVersionDialog();
-            }
-        });
+        if (regid.isEmpty()) {
+            registerInBackground(getApplicationContext());
+        } else {
+            sendRegistrationIdToBackend();
+        }
     }
 
 
@@ -89,5 +85,115 @@ public class LoadingActivity extends Activity {
         });
 
         builder.create().show();
+    }
+
+    private void registerInBackground(final Context context) {
+        new AsyncTask<String, Integer, String>() {
+            @Override
+            protected String doInBackground(String ... params) {
+                String msg = "";
+                try {
+                    GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
+                    String regid = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regid;
+
+                    // You should send the registration ID to your server over HTTP,
+                    // so it can use GCM/HTTP or CCS to send messages to your app.
+                    // The request to your server should be authenticated if your app
+                    // is using accounts.
+
+
+                    // For this demo: we don't need to send it because the device
+                    // will send upstream messages to a server that echo back the
+                    // message using the 'from' address in the message.
+
+                    // Persist the regID - no need to register again.
+                    storeRegistrationId(context, regid);
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                    // If there is an error, don't just keep trying to register.
+                    // Require the user to click a button again, or perform
+                    // exponential back-off.
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                Log.i("LOADING", result);
+                sendRegistrationIdToBackend();
+            }
+        }.execute(null, null, null);
+    }
+
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
+    private String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i("LOADING", "Registration not found.");
+            return "";
+        }
+        // Check if app was updated; if so, it must clear the registration ID
+        // since the existing regID is not guaranteed to work with the new
+        // app version.
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i("LOADING", "App version changed.");
+            return "";
+        }
+        return registrationId;
+    }
+
+    private SharedPreferences getGCMPreferences(Context context) {
+        // This sample app persists the registration ID in shared preferences, but
+        // how you store the regID in your app is up to you.
+        return getSharedPreferences("com.impulse", Context.MODE_PRIVATE);
+    }
+
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i("LOADING", "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+
+    private void sendRegistrationIdToBackend() {
+        String version = null;
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            version = pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        RestClient client = new RestClient();
+        client.initSession(version, new GetCallback() {
+            @Override
+            void onDataReceived(String response) {
+                if (response.equals(HttpStatus.SC_OK + "")) {
+                    mUploadingProgress.dismiss();
+                    Intent intent = new Intent(LoadingActivity.this, DrawerActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                } else
+                    showIncorrectVersionDialog();
+            }
+        });
     }
 }
